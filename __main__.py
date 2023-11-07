51c31e27333a574958fd77469245ef26f5d20b9c
hushuolin <113953125+hushuolin@users.noreply.github.com>
2023-11-07 03:14:03 -0500

    configure cloud-watch agent (#19)

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 """An AWS Python Pulumi program"""
 
 import pulumi
+import json
 import pulumi_aws as aws
 
 config = pulumi.Config()
@@ -33,6 +34,9 @@ DB_HOST={endpoint.split(":")[0]}
 USER_CSV_PATH=/opt/csye6225/webapp/users.csv
 EOL
 sudo chown -R csye6225:csye6225 /opt/csye6225/webapp
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/cloudwatch-config.json
+sudo systemctl daemon-reload
+sudo systemctl restart amazon-cloudwatch-agent
 sudo /usr/bin/node /opt/csye6225/webapp/index.js
 sleep 10
 sudo systemctl daemon-reload
@@ -223,6 +227,34 @@ db_instance = aws.rds.Instance("db-instance",
     tags={"Name": "db-instance"}
 )
 
+# Create IAM role for use with CloudAgent
+cloud_watch_role = aws.iam.Role("CloudWatchAgentServerRole",
+    assume_role_policy=json.dumps({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "sts:AssumeRole"
+            ],
+            "Principal": {
+                "Service": [
+                    "ec2.amazonaws.com"
+                ]
+            }
+        }
+    ]
+}))
+
+# Attach policy to role
+cloud_watch_role_policy_attachment = aws.iam.RolePolicyAttachment("cloud-watch-role-policy-attachment",
+    role=cloud_watch_role.name,
+    policy_arn="arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy")
+
+# Create IAM instance profile for attaching role to EC2
+cloud_watch_instance_profile = aws.iam.InstanceProfile("cloud-watch-instance-profile",
+    role=cloud_watch_role.name)
+
 # Launch an EC2 instance in one of the public subnets
 ec2_instance = aws.ec2.Instance('app-instance',
     instance_type=instanceType,
@@ -231,6 +263,7 @@ ec2_instance = aws.ec2.Instance('app-instance',
     user_data=create_user_data_script(db_instance.endpoint),  
     vpc_security_group_ids=[app_sg.id],
     subnet_id=public_subnets[0].id,  # Launch in the first public subnet
+    iam_instance_profile=cloud_watch_instance_profile.name,
     root_block_device=aws.ec2.InstanceRootBlockDeviceArgs(
         volume_size=volumeSize,
         volume_type=volumeType,
@@ -241,6 +274,17 @@ ec2_instance = aws.ec2.Instance('app-instance',
         "Name": "Webapp Instance",
     }
 )
+
+# Fetch the host zone dev.shuolin.me
+zone = aws.route53.get_zone(name="dev.shuolin.me")
+
+# Add or Update A record to Route53 zone
+A_record = aws.route53.Record("zone-record",
+    zone_id=zone.zone_id,
+    name="dev.shuolin.me",
+    type="A",
+    ttl=300,
+    records=[ec2_instance.public_ip])
 
 # Export the RDS instance endpoint
 pulumi.export('db_instance_endpoint', db_instance.endpoint)
